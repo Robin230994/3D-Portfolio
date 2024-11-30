d904575a20fc53c8033cb6e087e5bee27e780ead
RobinDort <robin@alexanderdort.com>
2024-11-30 12:25:18 +0100

    Added vite config

--- 3D-Portfolio/vite.config.ts
+++ 3D-Portfolio/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	build: {
+		outDir: "../dist", // Output in the dist/ folder
+		emptyOutDir: true, // Empty the folder first
+		sourcemap: true, // Add sourcemap
+	},
+	server: {
+		host: true, // Open to local network and display URL
+		open: !("SANDBOX_URL" in process.env || "CODESANDBOX_HOST" in process.env), // Open if it's not a CodeSandbox
+	},
+});
