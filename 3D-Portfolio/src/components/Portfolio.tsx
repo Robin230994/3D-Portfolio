693114909732aebdbcd4981e26b419ee76ac8b47
RobinDort <robin@alexanderdort.com>
2024-12-08 15:53:23 +0100

    Added a directionalLight as well as a helper

--- 3D-Portfolio/src/components/Portfolio.tsx
+++ 3D-Portfolio/src/components/Portfolio.tsx
@@ -1,10 +1,11 @@
-import { Center, useGLTF } from "@react-three/drei";
-import { DirectionalLight, Mesh } from "three";
+import { Center, useGLTF, useHelper } from "@react-three/drei";
+import { DirectionalLight, DirectionalLightHelper, Mesh } from "three";
 import { backWallMaterial, floorMaterial, frontWallMaterial, leftWallMaterial, windowWallMaterial } from "../Helper/materials";
 
 import CustomMesh from "./CustomMesh/CustomMesh";
 import MaterialCreator from "../classes/MaterialCreator";
 import useDebug from "../hooks/useDebug";
+import { MutableRefObject, useRef } from "react";
 
 type GLTFResult = {
 	nodes: {
@@ -15,15 +16,21 @@ type GLTFResult = {
 const materialCreator = MaterialCreator.getInstance();
 
 function Portfolio() {
+	/** Nodes / Meshes */
 	const { nodes } = useGLTF("./office-room.glb") as unknown as GLTFResult;
 	const { Lights } = useDebug(materialCreator);
+
 	const Sun = nodes["Sun"] as DirectionalLight;
+
+	/** REFS */
+	const sunlightRef = useRef<DirectionalLight | null>(null);
+	useHelper(sunlightRef as MutableRefObject<DirectionalLight>, DirectionalLightHelper, 1, "red");
 
 	return (
 		<>
 			<Center>
 				<ambientLight intensity={Lights.AmbientLight.intensity} />
-				<directionalLight intensity={Sun.intensity} position={Sun.position} rotation={Sun.rotation} color={Sun.color} />
+				<directionalLight intensity={1} position={Sun.position} rotation={Sun.rotation} color={Sun.color} ref={sunlightRef} />
 				{/************ Office Room ************/}
 				<group name="office-room">
 					{/************ BASE (Walls + Roof + Floor) ************/}
