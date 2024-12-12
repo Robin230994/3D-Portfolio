43cbd2a6b6cf4bcf4fa92326f5e8f1071decdae9
RobinDort <robin@alexanderdort.com>
2024-12-12 12:56:50 +0100

    Added missing door frame to the door component

--- 3D-Portfolio/src/components/Door/Door.tsx
+++ 3D-Portfolio/src/components/Door/Door.tsx
@@ -12,6 +12,7 @@ import MaterialCreator from "../../classes/MaterialCreator";
 
 const Door: React.FC<CustomMeshProps> = ({ name, nodes }) => {
 	const Door: Mesh = nodes["DoorBase"] as Mesh;
+	const DoorFrame: Mesh = nodes["DoorKnotFrame"] as Mesh;
 	const DoorHandleFP: Mesh = nodes["DoorHandleFF"] as Mesh;
 	const DoorHandleSP: Mesh = nodes["MainHandle"] as Mesh;
 
@@ -21,6 +22,9 @@ const Door: React.FC<CustomMeshProps> = ({ name, nodes }) => {
 			<mesh name={name} geometry={Door.geometry} position={Door.position} rotation={Door.rotation}>
 				<meshStandardMaterial color={"#7d5030"} side={DoubleSide} />
 			</mesh>
+
+			{/** Door Frame */}
+			<mesh geometry={DoorFrame.geometry} position={DoorFrame.position} rotation={DoorFrame.rotation} material={metalMaterial} />
 
 			{/** DoorHandle */}
 			<mesh geometry={DoorHandleFP.geometry} position={DoorHandleFP.position} rotation={DoorHandleFP.rotation} material={metalMaterial} />
