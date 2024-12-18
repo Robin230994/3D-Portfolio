6118f9d7ce06fca536a1db079d066ddc5f983e73
RobinDort <robin@alexanderdort.com>
2024-12-18 14:18:30 +0100

    Added new images for the iPhone and the macbook

--- 3D-Portfolio/src/components/Macbook/MacbookUI.tsx
+++ 3D-Portfolio/src/components/Macbook/MacbookUI.tsx
@@ -11,6 +11,15 @@ macbookHousingMaterial.metalness = metalMaterial.metalness;
 macbookHousingMaterial.color = new Color("#C0C0C0");
 macbookHousingMaterial.roughness = 0.35;
 
+const macbookScreenMaterial = materialCreator.createStandardMaterialFromTexture("MacbookScreen", {
+	diffuseT: "/baked-textures/Macbook/macbook_screen_logo_color_baked.jpg",
+	roughnessT: "/baked-textures/Macbook/macbook_screen_logo_roughness_baked.jpg",
+});
+
+const macbookKeysMaterial = materialCreator.createStandardMaterialFromTexture("MacbookKeys", {
+	diffuseT: "/baked-textures/Macbook/macbook_keys_color_baked.webp",
+});
+
 interface MacbookUIProps extends IUIComponentProps {
 	props: {
 		data: { myData: { name: string; nodes: { [key: string]: Mesh | DirectionalLight } } };
@@ -36,9 +45,7 @@ const MacbookUI: React.FC<MacbookUIProps> = ({ props }) => {
 			<mesh geometry={MacbookHousing.geometry} position={MacbookHousing.position} rotation={MacbookHousing.rotation} material={macbookHousingMaterial} />
 			<mesh geometry={MacbookScreen.geometry} position={MacbookScreen.position} rotation={MacbookScreen.rotation} material={macbookHousingMaterial} />
 			<mesh geometry={MacbookVentilation.geometry} position={MacbookVentilation.position} rotation={MacbookVentilation.rotation} material={metalMaterial} />
-			<mesh geometry={MacbookKeyboard.geometry} position={MacbookKeyboard.position} rotation={MacbookKeyboard.rotation}>
-				<meshStandardMaterial color={"black"} />
-			</mesh>
+			<mesh geometry={MacbookKeyboard.geometry} position={MacbookKeyboard.position} rotation={MacbookKeyboard.rotation} material={macbookKeysMaterial} />
 		</group>
 	);
 };
