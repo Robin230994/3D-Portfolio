f80832da358e5ac820d3b0d9cbe806a8a454eee9
RobinDort <75115265+RobinDort@users.noreply.github.com>
2025-08-20 11:13:00 +0200

    Added the window to the foundation

--- 3D-Portfolio/src/components/Foundation/Foundation.tsx
+++ 3D-Portfolio/src/components/Foundation/Foundation.tsx
@@ -1,23 +1,24 @@
 import { Mesh, MeshStandardMaterial } from "three";
 import { CustomMeshProps } from "../../interfaces/GLlnterfaces";
+import { glassMaterial } from "../../Helper/GLMaterials";
 import MaterialCreator from "../../classes/MaterialCreator";
 
 const materialCreator = MaterialCreator.getInstance();
-const foundationMaterial: MeshStandardMaterial = materialCreator.createStandardMaterialFromTexture(
-	"Foundation",
-	{
-		diffuseT: "/baked-textures/Foundation/foundation_t1_color.png",
-	},
-	true
-);
+const foundationMaterial: MeshStandardMaterial = materialCreator.createStandardMaterialFromTexture("Foundation", {
+	diffuseT: "/baked-textures/Foundation/foundation_t1_color.png",
+});
 
 const Foundation: React.FC<CustomMeshProps> = ({ name, nodes }) => {
 	const Foundation = nodes["foundation_t1"] as Mesh;
+	const Window = nodes["Window"] as Mesh;
 
 	return (
 		<group name={name}>
 			{/** Foundation */}
-			<mesh geometry={Foundation.geometry} position={Foundation.position} material={foundationMaterial} />
+			<mesh geometry={Foundation.geometry} position={Foundation.position} material={foundationMaterial} scale={Foundation.scale} />
+
+			{/** Window */}
+			<mesh geometry={Window.geometry} position={Window.position} rotation={Window.rotation} scale={Window.scale} material={glassMaterial} />
 		</group>
 	);
 };
