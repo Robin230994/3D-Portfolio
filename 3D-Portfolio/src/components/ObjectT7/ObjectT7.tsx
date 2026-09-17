8c8ea1fde90b12fd041b599c3a4d18c54e678af2
RobinDort <75115265+RobinDort@users.noreply.github.com>
2026-09-17 09:03:12 +0200

    Added the Mouse of the new Object

--- 3D-Portfolio/src/components/ObjectT7/ObjectT7.tsx
+++ 3D-Portfolio/src/components/ObjectT7/ObjectT7.tsx
@@ -1,22 +1,15 @@
 import { Material, Mesh } from "three";
 import { CustomMeshProps } from "../../interfaces/GLlnterfaces";
 
-import MaterialCreator from "../../classes/MaterialCreator";
-
-const materialCreator = MaterialCreator.getInstance();
-const t7Material = materialCreator.createStandardMaterialFromTexture("t7Material", {
-	diffuseT: "/baked-textures/Objects/objects_t8_color.jpg",
-	roughnessT: "/baked-textures/Objects/objects_t6_roughness.jpg",
-	metallnessT: "/baked-textures/Objects/objects_t6_metallness.jpg",
-});
-
 const ObjectT7: React.FC<CustomMeshProps> = ({ name, nodes }) => {
 	const ObjectT8: Mesh = nodes["object_t8"] as Mesh;
+	const Mouse: Mesh = nodes["Mouse"] as Mesh;
 	const ot7Material = ObjectT8.material as Material;
 
 	return (
 		<group name={name}>
 			<mesh geometry={ObjectT8.geometry} position={ObjectT8.position} rotation={ObjectT8.rotation} material={ot7Material} scale={ObjectT8.scale} />
+			<mesh geometry={Mouse.geometry} position={Mouse.position} rotation={Mouse.rotation} material={ot7Material} scale={Mouse.scale} />
 		</group>
 	);
 };
