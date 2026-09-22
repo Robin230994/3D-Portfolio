4e875fe6717bae86e86105e097f36988b59ce486
RobinDort <75115265+RobinDort@users.noreply.github.com>
2026-09-22 15:48:35 +0200

    Removed unnecessary instance of material creator

--- 3D-Portfolio/src/components/ObjectT1/ObjectT1.tsx
+++ 3D-Portfolio/src/components/ObjectT1/ObjectT1.tsx
@@ -1,19 +1,10 @@
 import { Mesh } from "three";
 import { CustomMeshProps } from "../../interfaces/GLlnterfaces";
 import { Material } from "three";
-import { useEffect } from "react";
-
-import MaterialCreator from "../../classes/MaterialCreator";
-
-const materialCreator = MaterialCreator.getInstance();
 
 const ObjectT1: React.FC<CustomMeshProps> = ({ name, nodes }) => {
 	const ObjectT1: Mesh = nodes["object_t1"] as Mesh;
 	const t1Material = ObjectT1.material as Material;
-
-	useEffect(() => {
-		materialCreator.addInstanciatedMaterial("t1Material", t1Material);
-	}, [t1Material]);
 
 	return (
 		<group name={name}>
