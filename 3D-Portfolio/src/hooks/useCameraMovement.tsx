e9e92ab74ff6aace8df74a6cf1873d4f9547cb6f
RobinDort <75115265+RobinDort@users.noreply.github.com>
2026-09-23 09:28:38 +0200

    Adjusted camera movement to prevent long snapping times

--- 3D-Portfolio/src/hooks/useCameraMovement.tsx
+++ 3D-Portfolio/src/hooks/useCameraMovement.tsx
@@ -7,8 +7,8 @@ import { CameraInfo } from "../types/GLTypes";
 import { cameraPresets } from "../Presets/Presets";
 import type { OrbitControls as OrbitControlsImpl } from "three-stdlib";
 
-const CAMERA_RETURN_SPEED = 0.01;
-const CAMERA_ARRIVAL_EPSILON = 0.01;
+const CAMERA_RETURN_SPEED = 0.02;
+const CAMERA_ARRIVAL_EPSILON = 0.05;
 const DEG2RAD = Math.PI / 180;
 
 const useCameraMovement = (controlsRef: React.RefObject<OrbitControlsImpl>) => {
